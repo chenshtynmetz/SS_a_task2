40e12e97f5ccad2338474439ad22a1bd07f00c2c
chenshtynmetz <93136824+chenshtynmetz@users.noreply.github.com>
2021-11-29 22:09:17 +0200

    commit 29\11-22:08

--- my_mat.h
+++ my_mat.h
@@ -2,8 +2,9 @@
 void make_matrix();
 //this function find the smaller number
 int find_minimum(int a, int b);
-//this function return if have trial between 2 קודקודים
+//this function return if have trial between 2 vertexes
 void have_trial();
-//this function return the short trial between 2 קודקודים
+//this function return the short trial between 2 vertexes
 int short_trial();
+//this function print the short trial between 2 vertexes
 void print_short_trial();
